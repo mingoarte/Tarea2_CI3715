20107b22f86fc9735e74020bf444bacc2337d068
mingoarte <domingoarte2850@gmail.com>
2017-01-24 11:48:52 -0400

    Ya contamos los dias y diferenciamos entre dias de semana y fines de semana.

---
+++ tarea2.py
@@ -0,0 +1,46 @@
+from datetime import *
+import datetime
+class Tarifa(object):
+
+	def __init__(self, tasa_dias_semana, tasa_fines_semana):
+		self.tasa_dias_semana = tasa_dias_semana
+		self.tasa_fines_semana = tasa_fines_semana
+		
+
+print('En que fecha entraste a trabajar? Introduce los datos sin espacio')
+fecha_entrada = input('Ej. 24-05-2011 ==> 24052011 \n')
+fecha_entrada = datetime.datetime.strptime(fecha_entrada, "%d%m%Y").date()
+
+
+
+print('En que fecha saliste del trabajo? Introduce los datos sin espacio')
+fecha_salida = input('Ej. 24-05-2011 ==> 24052011 \n')
+fecha_salida = datetime.datetime.strptime(fecha_salida, "%d%m%Y").date()
+
+total =  fecha_salida - fecha_entrada
+dias_semana = 0
+dias_fin_semana = 0
+
+for x in range(0,(fecha_salida - fecha_entrada).days):
+	dia = fecha_entrada + timedelta(x + 1)
+
+	if dia.weekday() >= 5:
+		
+		dias_fin_semana = dias_fin_semana + 1
+		print('Fin de semana ',dia)
+
+	else:
+		dias_semana = dias_semana + 1
+		print('Dia de semana',dia)
+
+
+
+
+print('Los dias de la semana son ', dias_semana)
+print('Los dias del fin de semana son ', dias_fin_semana)
+
+
+
+
+
+
