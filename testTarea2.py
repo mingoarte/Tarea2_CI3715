f451e2d05e00c62c054416ac220ebecc823dee47
edgarjsr <ejsr08@gmail.com>
2017-01-26 10:45:27 -0400

    Todas las pruebas que pude hacer

--- testTarea2.py
+++ testTarea2.py
@@ -8,9 +8,13 @@ import unittest
 from datetime import *
 import datetime
 
-from Tarea2 import Tarifa, fecha_salida, fecha_entrada
+from Tarea2 import *
 
 class TarifaTester(unittest.TestCase):
+    
+    def setUp(self):
+        self.tasa = Tarifa(tasa_dias_semana=1.0, tasa_fines_semana=2.0)
+        self.precio = calcularPrecio(self.tasa, fecha_entrada, fecha_salida)
     
     def testFechasValidas(self):
         self.assertTrue(not (fecha_salida < fecha_entrada), "La fecha de salida debe ser posterior a la de entrada.")
@@ -21,5 +25,16 @@ class TarifaTester(unittest.TestCase):
     def testRangoValidoU(self):
         self.assertTrue(not ((fecha_salida-fecha_entrada).seconds > 432000), "Sevicio maximo es de cinco dias")
         
-
-            
+    def testTasasValidas(self):
+        self.assertTrue(not (precio_normal <= 0), "Las tasas deben ser positivas.")
+        self.assertTrue(not (precio_fin_semana <= 0), "Las tasas deben ser positivas.")
+        
+    def testTasaFloat(self):
+        self.assertTrue(not (type(precio_normal) is not float), "Las tasas deben ser decimales.")
+        self.assertTrue(not (type(precio_fin_semana) is not float), "Las tasas deben ser decimales.")
+        
+    def testTasasDiferentes(self):
+        self.assertTrue(not (precio_normal == precio_fin_semana), "Las tasa diaria debe ser diferente a la de los fines de semana.")
+        
+    #def testPrecioFloat(self):
+    #    self.assertTrue(not (type(self.precio) is not float), "El precio a pagar debe ser decimal.")
