72e85846a28152033c78ac9e7eff3457b470ba34
mingoarte <domingoarte2850@gmail.com>
2017-01-26 09:05:01 -0400

    ya la funcion calcula el numero de horas y el precio

--- tarea2.py
+++ tarea2.py
@@ -1,3 +1,10 @@
+##
+##Tarea 2
+##Domingo Arteaga 11-10058
+##Edgar Silva 
+##
+
+
 from datetime import *
 import datetime
 class Tarifa(object):
@@ -6,6 +13,50 @@ class Tarifa(object):
 		self.tasa_dias_semana = tasa_dias_semana
 		self.tasa_fines_semana = tasa_fines_semana
 		
+
+
+def tiempoDeTrabajo(Tarifa,inicioDeServicio, finDeServicio):
+	dias_semana = 0
+	dias_fin_semana = 0
+	
+	for x in range(0,(finDeServicio - inicioDeServicio).days):
+		dia = inicioDeServicio + timedelta(x + 1)
+
+		if dia.weekday() >= 5:
+			
+			dias_fin_semana = dias_fin_semana + 1
+			print('Fin de semana ',dia)
+
+		else:
+			dias_semana = dias_semana + 1
+			print('Dia de semana',dia)
+
+
+	if inicioDeServicio.date().weekday() >= 5:
+		gasto_horas_entrada = (24 - int(inicioDeServicio.time().hour)) * Tarifa.tasa_fines_semana
+	else: 
+		gasto_horas_entrada = (24 - int(inicioDeServicio.time().hour)) * Tarifa.tasa_dias_semana
+
+
+	if finDeServicio.date().weekday() >= 5:
+		gasto_horas_salida = int(finDeServicio.time().hour) * Tarifa.tasa_fines_semana
+	else: 
+		gasto_horas_salida = int(finDeServicio.time().hour) * Tarifa.tasa_dias_semana
+
+
+	total = gasto_horas_salida + gasto_horas_entrada + (dias_semana * 24 * Tarifa.tasa_dias_semana)\
+								 + (dias_fin_semana * 24 * Tarifa.tasa_fines_semana)
+	
+
+	print('El monto total es: Bs. ', total)
+
+
+precio_normal = input('Cual es la tarifa para los dias de semana?')
+precio_fin_semana = input('Cual es la tarifa para los fines de semana?')
+
+precios = Tarifa(int(precio_normal) , int(precio_fin_semana))
+
+
 format = '%d-%m-%Y %H:%M'
 
 print('En que fecha entraste a trabajar? Introduce los datos sin espacio')
@@ -19,42 +70,9 @@ fecha_salida = input('Ej. 24-05-2011 23:44 \n')
 fecha_salida = datetime.datetime.strptime(fecha_salida, format)
 
 
-dias_semana = 0
-dias_fin_semana = 0
 
 
-for x in range(0,(fecha_salida - fecha_entrada).days):
-	dia = fecha_entrada + timedelta(x + 1)
-
-	if dia.weekday() >= 5:
-		
-		dias_fin_semana = dias_fin_semana + 1
-		print('Fin de semana ',dia)
-
-	else:
-		dias_semana = dias_semana + 1
-		print('Dia de semana',dia)
-
-
-if fecha_entrada.date().weekday() >= 5:
-	gasto_horas_entrada = (24 - int(fecha_entrada.time().hour)) 
-else: 
-	gasto_horas_entrada = (24 - int(fecha_entrada.time().hour)) 
-
-
-if fecha_salida.date().weekday() >= 5:
-	gasto_horas_salida = int(fecha_salida.time().hour)
-else: 
-	gasto_horas_salida = int(fecha_salida.time().hour)
-
-
-total = gasto_horas_salida + gasto_horas_entrada + (dias_semana * 24) + (dias_fin_semana * 24)
-print('Los dias de la semana son ', dias_semana)
-print('Los dias del fin de semana son ', dias_fin_semana)
-print('El total de horas es : ', total)
-
-
-
+tiempoDeTrabajo(precios, fecha_entrada,fecha_salida)
 
 
 
